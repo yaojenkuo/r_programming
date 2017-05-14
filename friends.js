84ed1aa63ec7ef70b6a99c862770bb57eec4c41b
yaojenkuo <tonykuoyj@gmail.com>
2017-05-14 13:01:04 +0800

    first commit

---
+++ friends.js
@@ -0,0 +1,9 @@
+var friends_json = {
+  "genre": "Sitcom",
+  "seasons": 10,
+  "episodes": 236,
+  "stars": ["Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "Matt LeBlanc", "Matthew Perry", "David Schwimmer"]
+}
+
+console.log("六人行有幾季：" + friends_json["seasons"])
+console.log("Who stars Rachel Green: " + friends_json["stars"][0])
---
+++ friends_stars.js
@@ -0,0 +1,10 @@
+var starring_json = [
+  {"character": "Rachel Green", "star": "Jennifer Aniston"},
+  {"character": "Monica Geller", "star": "Courteney Cox"},
+  {"character": "Phoebe Buffay", "star": "Lisa Kudrow"},
+  {"character": "Joey Tribbiani", "star": "Matt LeBlanc"},
+  {"character": "Chandler Bing", "star": "Matthew Perry"},
+  {"character": "Ross Geller", "star": "David Schwimmer"}
+];
+
+console.log("Who stars " + starring_json[0]["character"] + ": " + starring_json[0]["star"])
